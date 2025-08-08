b5584ed45e2ea3ec59ccceada92d63aad0a622f9
86131 <1751249755@qq.com>
2025-08-08 19:33:42 +0800

    增加自动更新

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -46,7 +46,7 @@ export default {
       messages: [],
       newMsg: '',
       timer: null,
-       autoRefresh: false   // true 开启，false 关闭
+       autoRefresh: true   // true 开启，false 关闭
     }
   },
   created() {
